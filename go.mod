689ef3a8a21ee39a40970f209ad192bbf35a2133
Rafaela <rafaela.ester.lf@gmail.com>
2023-06-29 00:08:22 -0300

    initial commit

--- go.mod
+++ go.mod
@@ -9,8 +9,12 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgx/v5 v5.2.0 // indirect
 	github.com/jackc/puddle/v2 v2.1.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
 	golang.org/x/text v0.3.8 // indirect
+	gorm.io/gorm v1.25.1 // indirect
 )
--- url/api.go
+++ url/api.go
@@ -4,55 +4,224 @@ import (
 	"context"
 	"encore.dev/storage/sqldb"
 	"errors"
+	_ "gorm.io/gorm"
 )
 
+//estrutura dos parametros do usuario
+
 type User struct {
+	ID    int
 	Name  string
 	Email string
 	Role  string
 }
 
+// estrutura dos parametros para excluir usuario
+
+type UserDelete struct {
+	ID   int
+	Name string
+}
+
+// estrutura dos parametros para atualizar usuario
+
+type UpdateUser struct {
+	ID      int
+	Name    string
+	Email   string
+	Role    string
+	NewRole string
+}
+
+// estrutura dos parametros para ler um usuario
+
+type ReadUser struct {
+	ID       string
+	ReadUser *User
+}
+
+// estrutura de retorno para o usuario
+
+type CreateUserResponse struct {
+	Message string
+}
+
+// implementado o ednpoint de criação de cadastro de usuario, utilizadno a estrutura de usuario que foi definida em cima
+
 //encore:api public method=POST path=/users/create
-func createUser(ctx context.Context, user *User) error {
+func createUser(ctx context.Context, user *User) (*CreateUserResponse, error) {
 	if user.Name == "" || user.Email == "" || user.Role == "" {
-		return errors.New("Name, Email and Role are required fields")
+		return nil, errors.New("Name, Email and Role are required fields")
 	}
 
 	if user.Role != "Admin" && user.Role != "Modifier" && user.Role != "Watcher" {
-		return errors.New("Role diferent!")
+		return nil, errors.New("Invalid role. Allowed roles are Admin, Modifier, and Watcher")
 	}
 
 	existingUser, err := existingUser(ctx, user.Name, user.Email, user.Role)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if existingUser {
-		return errors.New("User already exists, try again!")
+		return nil, errors.New("User already exists!")
 	}
 
-	err = inserteUsers(ctx, user.Name, user.Email, user.Role)
+	_, err = sqldb.Exec(ctx, `
+		INSERT INTO users (name, email, role)
+		VALUES ($1, $2, $3)
+	`, user.Name, user.Email, user.Role)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return errors.New("User created successfully")
+
+	response := &CreateUserResponse{
+		Message: "User created successfully",
+	}
+
+	return response, nil
 }
+
+// Estrutura de leitura de usuario, ao inserir o id a api busca se existe ou nao, caso exista retorna na tela os dados do usuario.
+
+//encore:api public method=GET path=/read/:id
+func readUser(ctx context.Context, id string) (*ReadUser, error) {
+	u := &ReadUser{ID: id}
+	user := &User{}
+
+	err := sqldb.QueryRow(ctx, `
+		SELECT id, name, email, role FROM users
+		WHERE id = $1
+	`, id).Scan(&user.ID, &user.Name, &user.Email, &user.Role)
+	if err != nil {
+		return nil, errors.New("User already exists!")
+	}
+
+	u.ReadUser = user
+	return u, nil
+}
+
+type ListUsersResponse struct {
+	Users []*User
+}
+
+// Retorna os dados de todos os usuarios da api
+
+//encore:api public method=GET path=/users
+func ListUsers(ctx context.Context) (*ListUsersResponse, error) {
+	rows, err := sqldb.Query(ctx, `
+		SELECT id, name, email, role FROM users
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := []*User{}
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.Role); err != nil {
+			return nil, err
+		}
+		users = append(users, &u)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	response := &ListUsersResponse{
+		Users: users,
+	}
+
+	return response, nil
+}
+
+type UpdateUserResponse struct {
+	Message string
+}
+
+// endpoint desenvolvido para realizar o update dos usuarios, a estrtutura foi definida no inicio. Ao iformar
+// os dados é preciso enviar a nova função para as verificações
+
+//encore:api public method=POST path=/UpdateUser/update
+func updateUser(ctx context.Context, update *UpdateUser) (*UpdateUserResponse, error) {
+	if update.Name == "" || update.Email == "" || update.Role == "" || update.NewRole == "" {
+		return nil, errors.New("Name, Email, Role and New Role are required fields")
+	}
+
+	if update.Role != "Admin" && update.Role != "Modifier" && update.Role != "Watcher" {
+		return nil, errors.New("Invalid role. Allowed roles are Admin, Modifier, and Watcher")
+	}
+
+	if update.Role != update.NewRole {
+		existingUser, err := existingUser(ctx, update.Name, update.Email, update.Role)
+		if err != nil {
+			return nil, err
+		}
+		if existingUser {
+
+			if update.Role == "Admin" && (update.NewRole == "Watcher" || update.NewRole == "Modifier") {
+				err = updateUsers(ctx, update.Name, update.Email, update.NewRole)
+				if err != nil {
+					return nil, err
+				}
+			} else if update.Role == "Modifier" && update.NewRole == "Watcher" {
+				err = updateUsers(ctx, update.Name, update.Email, update.NewRole)
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				return nil, errors.New("User cannot change roles.")
+			}
+
+		} else {
+			return nil, errors.New("User does not exist")
+		}
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, errors.New("New Role must be different from the current role")
+	}
+
+	responseUpdate := &UpdateUserResponse{
+		Message: "User updated successfully",
+	}
+	return responseUpdate, nil
+}
+
+type DeleteUserResponse struct {
+	Message string
+}
+
+//Neste endpoint é recuperado os dados que o foram informados e feito a consulta no banco para a remoção do usuario.
 
 //encore:api public method=POST path=/users/delete
-func deleteUser(ctx context.Context, user *User) error {
-	if user.Name == "" || user.Email == "" || user.Role == "" {
-		return errors.New("Name, Email and Role are required fields")
+func deleteUser(ctx context.Context, user *UserDelete) (*DeleteUserResponse, error) {
+	if user.ID == 0 || user.Name == "" {
+		return nil, errors.New("ID and Name are required fields!")
 	}
-	err := deleteUsers(ctx, user.Name, user.Email, user.Role)
+
+	err, _ := sqldb.Exec(ctx, `
+		DELETE FROM users WHERE id = $1 AND name = $2
+	`, user.ID, user.Name)
 	if err != nil {
-		return err
+		return nil, errors.New("User does not exist")
 	}
-	return nil
+	response := &DeleteUserResponse{
+		Message: "User deleted successfully",
+	}
+	return response, nil
+
 }
+
+//Execuçoes no banco de dados, essas funções sao chamadas no corpo do endpoint. A funcao de "ExistingUser" é utilizada
+//em todas as outras funcoes, ois pe feito a verificação em cada etapa se já existe cadastro.
 
 func existingUser(ctx context.Context, name string, email string, role string) (bool, error) {
 	var count int
 	err := sqldb.QueryRow(ctx, `
-		SELECT COUNT(*) FROM users WHERE name = $1 AND email = $2 AND role = $3
+		SELECT COUNT(*) FROM users WHERE name = $1 and  email = $2 and role = $3
 	`, name, email, role).Scan(&count)
 	if err != nil {
 		return false, err
@@ -61,21 +230,10 @@ func existingUser(ctx context.Context, name string, email string, role string) (
 	return count > 0, nil
 }
 
-func deleteUsers(ctx context.Context, name string, email string, role string) error {
+func updateUsers(ctx context.Context, name string, email string, role string) error {
 	_, err := sqldb.Exec(ctx, `
-		DELETE FROM users WHERE name = $1 AND email = $2 AND role = $3
-	`, name, email, role)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func inserteUsers(ctx context.Context, name string, email string, role string) error {
-	_, err := sqldb.Exec(ctx, `
-		INSERT INTO users (name, email, role)
-		VALUES ($1, $2, $3)
+		UPDATE users SET role = $3
+		WHERE name = $1 AND email = $2
 	`, name, email, role)
 	return err
 }
